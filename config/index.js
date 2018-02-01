9eb3c569afb33cbd5dd562d27c7b0bb764788862
gjw <122486975@qq.com>
2018-02-01 15:26:53 +0800

    修改webpack配置

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -71,7 +70,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist',
 
     /**
      * Source Maps
